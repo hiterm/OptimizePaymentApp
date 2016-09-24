908db84ff30c3a53bd7cdd43211ad2ee4623bede
htlsne <htlsne@users.noreply.github.com>
2016-09-24 23:01:50 +0900

    Remove comments

--- app/src/main/java/io/github/htlsne/optimizepayment/SetWalletAmountActivity.java
+++ app/src/main/java/io/github/htlsne/optimizepayment/SetWalletAmountActivity.java
@@ -82,20 +82,6 @@ public class SetWalletAmountActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
-
         switch (id) {
             case R.id.nav_payment:
                 startActivity(new Intent(this, MainActivity.class));
