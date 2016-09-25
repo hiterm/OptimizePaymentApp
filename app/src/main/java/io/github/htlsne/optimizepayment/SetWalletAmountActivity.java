154a10a1a60030b8321eff9fd2308d7875c37e4a
htlsne <htlsne@users.noreply.github.com>
2016-09-25 18:45:07 +0900

    Add SetWalletAmountActivity

---
+++ app/src/main/java/io/github/htlsne/optimizepayment/SetWalletAmountActivity.java
@@ -0,0 +1,13 @@
+package io.github.htlsne.optimizepayment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetWalletAmountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_wallet_amount);
+    }
+}
